81a91c933bef240401aaca16c261fc219bec211e
최한슬 <hanseulo@naver.com>
2023-07-28 14:59:19 +0900

    Node.js 23-07-28

---
+++ Node/EX02/index.js
@@ -0,0 +1,19 @@
+/*
+1. module 종류
+1-1. 사용자 정의 모듈
+1-2. node.js 기본 모듈
+1-3. 외부 모듈
+
+2. 사용자 정의 모듈
+1-1. 변수,객체,함수,폴더를 module로 만듦
+1-2. 외부로 공개 module.exports == exports
+1-3. reqire을 사용해 module을 사용
+*/
+
+const sum = require("./sum");
+const sum_result = sum(1, 2);
+console.log(sum_result);
+
+const mul = require("./mul");
+const mul_result = mul(1, 2);
+console.log(mul_result);
---
+++ Node/EX02/sum.js
@@ -0,0 +1,8 @@
+// 모듈선언: 필요하면 외부파일에서 가져다 사용해
+
+let sum = function (su, num) {
+    return su + num;
+};
+
+// module.exports = sum; 또는
+module.exports = sum;
